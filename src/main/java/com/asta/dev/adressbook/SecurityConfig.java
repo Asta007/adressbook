5e7c8f4035ee93e5d5d7ff3e0f6a60df7c9e9514
Asta <asta.hb2xy@gmail.com>
2025-02-22 22:11:08 +0000

    Authentication success

---
+++ src/main/java/com/asta/dev/adressbook/SecurityConfig.java
@@ -0,0 +1,38 @@
+package com.asta.dev.adressbook;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+                .authorizeRequests()
+                .requestMatchers("/").permitAll()
+                .anyRequest().authenticated();
+        http
+                .oauth2ResourceServer()
+                .jwt();
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        return http.build();
+    }
+
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception {
+//        http
+//                .csrf().disable()
+//                .authorizeRequests()
+//                .antMatchers("/", "/home").permitAll() // Accès libre à l'index et home
+//                .antMatchers("/contact/**").hasRole("user") // Protéger les endpoints de contact
+//                .anyRequest().authenticated()
+//                .and()
+//                .oauth2Login();
+//    }
+}
